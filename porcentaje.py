0765ace05ac193aa7d8caf104473b06cd5008310
Carlos Mantilla <carlos.mantilla@tecnologicoliceocristiano.edu.ec>
2024-07-23 19:26:26 -0500

    first commit

---
+++ porcentaje.py
@@ -0,0 +1,12 @@
+# Escribe un programa que solicite al usuario
+# un número y le reste el 15%, almacenando todo en una única variable
+# A continuación, mostrar el resultado final en pantalla
+
+Numero = float(input("Ingrese el numero que quiere restar: "))
+
+Resultado = Numero * 15/100
+
+Resta = Numero - Resultado
+
+print("El resultado del número restado por el 15% : "+ str(Resta))
+
