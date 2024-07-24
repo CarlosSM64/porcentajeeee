011b9cef303bb3d3760aa022610c2f62f15dccfc
Carlos Mantilla <carlos.mantilla@tecnologicoliceocristiano.edu.ec>
2024-07-23 21:06:56 -0500

    ejercicios variados

---
+++ Un articulo.py
@@ -0,0 +1,20 @@
+# Escriba un programa que le solicite al usuario su edad y la guarde en una varible
+# que luego solicite la cantidad de articulos comprados en una tienda
+# y la guarde en otro variable. FInalmente, mostrar en pantalla un valor
+# de verdad (True or False) que indique si el usuario es mayor de 
+# 18 años de edad y además que compro más de 1 artículo
+
+Edad = int(input("Ingrese su edad: "))
+Articulos = int(input("Ingrese articulos comprados: "))
+
+if (Edad >= 18 ):
+    print("True")
+else:
+    print("False")
+
+if (Articulos >= 1):
+    print("True")
+else:
+    print("False")
+    
+
